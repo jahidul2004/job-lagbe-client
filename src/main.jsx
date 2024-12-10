c702b5fffec6cd06aa1c2eebf1ba559361f4cb45
jahidul2004 <islamjahiduljihad@gmail.com>
2024-12-10 18:43:08 +0600

    Project organaize by using external router folder.

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.jsx";
+import router from "./router/Router.jsx";
+import { RouterProvider } from "react-router-dom";
 
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-
-const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <div>Initial setup done</div>,
-    },
-]);
 
 createRoot(document.getElementById("root")).render(
     <StrictMode>
