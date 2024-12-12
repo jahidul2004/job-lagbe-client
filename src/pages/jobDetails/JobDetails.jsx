c105533c41ea78335eef7a137593af0630c3bcf1
jahidul2004 <islamjahiduljihad@gmail.com>
2024-12-12 23:12:12 +0600

    Similar jobs section added.

--- src/pages/jobDetails/JobDetails.jsx
+++ src/pages/jobDetails/JobDetails.jsx
@@ -1,3 +1,5 @@
+import { div } from "motion/react-client";
+import { useEffect, useState } from "react";
 import { BiSolidWatch } from "react-icons/bi";
 import { CiLocationOn } from "react-icons/ci";
 import { FiWatch } from "react-icons/fi";
@@ -9,7 +11,7 @@ import {
     MdOutlineUpdate,
 } from "react-icons/md";
 import { PiBagBold, PiBuildingOfficeFill } from "react-icons/pi";
-import { useLoaderData } from "react-router-dom";
+import { Link, useLoaderData } from "react-router-dom";
 
 const JobDetails = () => {
     const data = useLoaderData();
@@ -27,8 +29,22 @@ const JobDetails = () => {
         status,
         company_logo,
         hr_email,
-        hr_name,
     } = data;
+
+    const [similarJobs, setSimilarJobs] = useState([]);
+
+    useEffect(() => {
+        fetch(`http://localhost:5000/jobs`)
+            .then((res) => res.json())
+            .then((data) => {
+                // Randomly shuffle the jobs and select 5
+                const randomJobs = data
+                    .sort(() => 0.5 - Math.random())
+                    .slice(0, 5);
+                setSimilarJobs(randomJobs);
+            })
+            .catch((error) => console.error("Error fetching jobs:", error));
+    }, []);
 
     return (
         <div className="container px-4 md:mx-auto my-10">
@@ -219,45 +235,85 @@ const JobDetails = () => {
                     </div>
                 </div>
 
-                <div className="col-span-1 border p-4 rounded-lg h-max">
-                    {/* Header area */}
-                    <div className="flex gap-2 border-b pb-2">
-                        <div className="w-[50px] h-[50px] border rounded-lg flex justify-center items-center p-1">
-                            <img src={company_logo} alt="" />
+                <div className="col-span-1">
+                    <div className="border p-4 rounded-lg h-max">
+                        {/* Header area */}
+                        <div className="flex gap-2 border-b pb-2">
+                            <div className="w-[50px] h-[50px] border rounded-lg flex justify-center items-center p-1">
+                                <img src={company_logo} alt="" />
+                            </div>
+                            <div>
+                                <h1 className="text-xl font-bold">{company}</h1>
+                                <p className="text-[#000000ab] flex items-center font-semibold">
+                                    <GrLocation />
+                                    {location}
+                                </p>
+                            </div>
                         </div>
+                        {/* Header area end */}
+
+                        {/* Maps area */}
                         <div>
-                            <h1 className="text-xl font-bold">{company}</h1>
-                            <p className="text-[#000000ab] flex items-center font-semibold">
-                                <GrLocation />
-                                {location}
-                            </p>
+                            <iframe
+                                className="w-full h-[200px] mt-4 rounded-lg"
+                                src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d29216.091936364286!2d90.4200192!3d23.746969599999996!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sbd!4v1734015779418!5m2!1sen!2sbd"
+                                width="600"
+                                height="450"
+                                allowfullscreen=""
+                                loading="lazy"
+                                referrerpolicy="no-referrer-when-downgrade"
+                            ></iframe>
+                        </div>
+                        {/* Maps area end */}
+
+                        {/* Address area */}
+                        <div className="mt-5">
+                            <ul className="list-inside list-disc font-semibold text-[#000000ab]">
+                                <li>{location}</li>
+                                <li>Phone:(123) 456-7890 C.G</li>
+                                <li>Email: {hr_email}</li>
+                            </ul>
+                        </div>
+                        {/* Address area end */}
+                    </div>
+
+                    <div className="border rounded-lg p-4 mt-4">
+                        <h1 className="text-xl font-bold border-b pb-2">
+                            Similar Jobs
+                        </h1>
+                        <div className="flex flex-col gap-2 mt-4">
+                            {similarJobs.map((job) => (
+                                <div className="flex justify-between items-center gap-2 border p-2 rounded-lg">
+                                    <div className="flex gap-2 items-center">
+                                        <div className="w-[50px] h-[50px] border p-1 rounded-lg">
+                                            <img
+                                                className="w-full h-full"
+                                                src={job.company_logo}
+                                                alt=""
+                                            />
+                                        </div>
+                                        <div>
+                                            <h1 className="text-lg font-bold">
+                                                {job.title}
+                                            </h1>
+                                            <p className="text-[#000000ab] flex items-center font-semibold">
+                                                <GrLocation />
+                                                {job.location}
+                                            </p>
+                                        </div>
+                                    </div>
+                                    <div>
+                                        <Link
+                                            to={`/jobs/${job._id}`}
+                                            className="btn btn-success text-white font-bold"
+                                        >
+                                            Apply
+                                        </Link>
+                                    </div>
+                                </div>
+                            ))}
                         </div>
                     </div>
-                    {/* Header area end */}
-
-                    {/* Maps area */}
-                    <div>
-                        <iframe
-                            className="w-full h-[200px] mt-4 rounded-lg"
-                            src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d29216.091936364286!2d90.4200192!3d23.746969599999996!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sbd!4v1734015779418!5m2!1sen!2sbd"
-                            width="600"
-                            height="450"
-                            allowfullscreen=""
-                            loading="lazy"
-                            referrerpolicy="no-referrer-when-downgrade"
-                        ></iframe>
-                    </div>
-                    {/* Maps area end */}
-
-                    {/* Address area */}
-                    <div className="mt-5">
-                        <ul className="list-inside list-disc font-semibold text-[#000000ab]">
-                            <li>{location}</li>
-                            <li>Phone:(123) 456-7890 C.G</li>
-                            <li>Email: {hr_email}</li>
-                        </ul>
-                    </div>
-                    {/* Address area end */}
                 </div>
             </div>
             {/* Job description area end */}
