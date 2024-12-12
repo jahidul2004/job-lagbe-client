1bdc5de143e81e1aa2634c46259e61f5aaa5f465
jahidul2004 <islamjahiduljihad@gmail.com>
2024-12-12 22:48:24 +0600

    Details page more details added.

--- src/pages/jobDetails/JobDetails.jsx
+++ src/pages/jobDetails/JobDetails.jsx
@@ -68,74 +68,158 @@ const JobDetails = () => {
 
             {/* Job description area */}
             <div className="grid grid-cols-1 md:grid-cols-3 gap-8 mt-12">
-                <div className="col-span-1 md:col-span-2 border p-4 rounded-lg h-max">
-                    <div className="border-b py-4">
-                        <h1 className="text-xl font-bold">
-                            Employment Information
-                        </h1>
+                <div className="col-span-1 md:col-span-2">
+                    <div className=" border p-4 rounded-lg h-max">
+                        <div className="border-b py-4">
+                            <h1 className="text-xl font-bold">
+                                Employment Information
+                            </h1>
+                        </div>
+                        <div className="grid grid-cols-1 md:grid-cols-2 text-lg font-semibold gap-6 py-4">
+                            <div className="flex items-center gap-2">
+                                <span className="flex items-center gap-4 text-[#000000ab]">
+                                    <PiBuildingOfficeFill className="text-[#008854]" />
+                                    Industry:
+                                </span>
+                                {company}
+                            </div>
+                            <div className="flex items-center gap-2">
+                                <span className="flex items-center gap-4 text-[#000000ab]">
+                                    <LuBadgeDollarSign className="text-[#008854]" />
+                                    Salary:
+                                </span>
+                                {salaryRange.min} - {salaryRange.max}
+                                {salaryRange.currency.toUpperCase()}
+                            </div>
+                            <div className="flex items-center gap-2">
+                                <span className="flex items-center gap-4 text-[#000000ab]">
+                                    <MdOutlineTypeSpecimen className="text-[#008854]" />
+                                    Job Type:
+                                </span>
+                                {jobType}
+                            </div>
+                            <div className="flex items-center gap-2">
+                                <span className="flex items-center gap-4 text-[#000000ab]">
+                                    <MdOutlineUpdate className="text-[#008854]" />
+                                    Updated:
+                                </span>
+                                {status}
+                            </div>
+                            <div className="flex items-center gap-2">
+                                <span className="flex items-center gap-4 text-[#000000ab]">
+                                    <MdOutlineManageAccounts className="text-[#008854]" />
+                                    Job Level:
+                                </span>
+                                Experienced C.G
+                            </div>
+                            <div className="flex items-center gap-2">
+                                <span className="flex items-center gap-4 text-[#000000ab]">
+                                    <GrUserExpert className="text-[#008854]" />
+                                    Experience:
+                                </span>
+                                1 - 2 years.C.G
+                            </div>
+                            <div className="flex items-center gap-2">
+                                <span className="flex items-center gap-4 text-[#000000ab]">
+                                    <FiWatch className="text-[#008854]" />
+                                    Deadline:
+                                </span>
+                                {applicationDeadline}
+                            </div>
+                            <div className="flex items-center gap-2">
+                                <span className="flex items-center gap-4 text-[#000000ab]">
+                                    <GrLocation className="text-[#008854]" />
+                                    Location:
+                                </span>
+                                {location}
+                            </div>
+                        </div>
                     </div>
-                    <div className="grid grid-cols-1 md:grid-cols-2 text-lg font-semibold gap-6 py-4">
-                        <div className="flex items-center gap-2">
-                            <span className="flex items-center gap-4 text-[#000000ab]">
-                                <PiBuildingOfficeFill className="text-[#008854]" />
-                                Industry:
-                            </span>
-                            {company}
+
+                    <div className="border rounded-lg p-4 mt-4">
+                        <div>
+                            <h1 className="text-xl font-bold mb-2">
+                                Welcome to Job Lagbe
+                            </h1>
+                            <p className="text-[#000000ab]">
+                                Job Lagbe is a modern and user-friendly job
+                                portal designed to create a reliable connection
+                                between job seekers and employers. It offers
+                                users the ability to explore job opportunities
+                                across various industries, apply with ease, and
+                                manage their job applications efficiently.
+                                Employers can post job listings, review
+                                applications, and find the ideal candidates for
+                                their requirements.
+                            </p>
                         </div>
-                        <div className="flex items-center gap-2">
-                            <span className="flex items-center gap-4 text-[#000000ab]">
-                                <LuBadgeDollarSign className="text-[#008854]" />
-                                Salary:
-                            </span>
-                            {salaryRange.min} - {salaryRange.max}
-                            {salaryRange.currency.toUpperCase()}
+                        <div className="mt-4">
+                            <h1 className="text-xl font-bold mb-2">
+                                Essential Knowledge, Skills, and Experience
+                            </h1>
+                            <ul className="list-disc list-inside text-[#000000ab]">
+                                <li>
+                                    A portfolio demonstrating
+                                    well-thought-through and polished end-to-end
+                                    customer journeys
+                                </li>
+                                <li>
+                                    5+ years of industry experience in
+                                    interactive design and/or visual design
+                                </li>
+                                <li>Excellent interpersonal skills</li>
+                                <li>
+                                    Aware of trends in mobile, communications,
+                                    and collaboration
+                                </li>
+                                <li>
+                                    Ability to create highly polished design
+                                    prototypes, mockups, and other communication
+                                    artifacts
+                                </li>
+                                <li>
+                                    The ability to scope and estimate efforts
+                                    accurately and prioritize tasks and goals
+                                    independently
+                                </li>
+                                <li>
+                                    History of impacting shipping products with
+                                    your work
+                                </li>
+                                <li>
+                                    A Bachelor's Degree in Design (or related
+                                    field) or equivalent professional experience
+                                </li>
+                                <li>
+                                    Proficiency in a variety of design tools
+                                    such as Figma, Photoshop, Illustrator, and
+                                    Sketch
+                                </li>
+                            </ul>
                         </div>
-                        <div className="flex items-center gap-2">
-                            <span className="flex items-center gap-4 text-[#000000ab]">
-                                <MdOutlineTypeSpecimen className="text-[#008854]" />
-                                Job Type:
-                            </span>
-                            {jobType}
-                        </div>
-                        <div className="flex items-center gap-2">
-                            <span className="flex items-center gap-4 text-[#000000ab]">
-                                <MdOutlineUpdate className="text-[#008854]" />
-                                Updated:
-                            </span>
-                            {status}
-                        </div>
-                        <div className="flex items-center gap-2">
-                            <span className="flex items-center gap-4 text-[#000000ab]">
-                                <MdOutlineManageAccounts className="text-[#008854]" />
-                                Job Level:
-                            </span>
-                            Experienced C.G
-                        </div>
-                        <div className="flex items-center gap-2">
-                            <span className="flex items-center gap-4 text-[#000000ab]">
-                                <GrUserExpert className="text-[#008854]" />
-                                Experience:
-                            </span>
-                            1 - 2 years.C.G
-                        </div>
-                        <div className="flex items-center gap-2">
-                            <span className="flex items-center gap-4 text-[#000000ab]">
-                                <FiWatch className="text-[#008854]" />
-                                Deadline:
-                            </span>
-                            {applicationDeadline}
-                        </div>
-                        <div className="flex items-center gap-2">
-                            <span className="flex items-center gap-4 text-[#000000ab]">
-                                <GrLocation className="text-[#008854]" />
-                                Location:
-                            </span>
-                            {location}
+                        <div className="mt-4">
+                            <h1 className="text-xl font-bold">
+                                Preferred Experience
+                            </h1>
+                            <ul className="list-disc list-inside text-[#000000ab]">
+                                <li>
+                                    Designing user experiences for enterprise
+                                    software/services
+                                </li>
+                                <li>
+                                    Creating and applying established design
+                                    principles and interaction patterns
+                                </li>
+                                <li>
+                                    Aligning or influencing design thinking with
+                                    teams working in other geographies
+                                </li>
+                            </ul>
                         </div>
                     </div>
                 </div>
 
-                <div className="col-span-1 border p-4 rounded-lg">
+                <div className="col-span-1 border p-4 rounded-lg h-max">
                     {/* Header area */}
                     <div className="flex gap-2 border-b pb-2">
                         <div className="w-[50px] h-[50px] border rounded-lg flex justify-center items-center p-1">
