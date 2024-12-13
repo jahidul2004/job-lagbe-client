d8119de77919834ce14a9c2b75db4b097286005e
jahidul2004 <islamjahiduljihad@gmail.com>
2024-12-13 17:40:39 +0600

    All jobs addrd.

--- src/pages/allJobs/AllJobs.jsx
+++ src/pages/allJobs/AllJobs.jsx
@@ -1,8 +1,19 @@
+import { useLoaderData } from "react-router-dom";
+import HotJobCard from "../home/HotJobCard";
 
 const AllJobs = () => {
+    const data = useLoaderData();
+
     return (
-        <div>
-            this is all jobs
+        <div className="px-5 my-10">
+            <h1 className="text-3xl font-bold text-center text-success my-5 pb-5">
+                All Jobs
+            </h1>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+                {data.map((job) => (
+                    <HotJobCard key={job._id} job={job} />
+                ))}
+            </div>
         </div>
     );
 };
