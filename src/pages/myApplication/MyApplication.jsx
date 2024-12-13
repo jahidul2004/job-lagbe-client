6a999a36cd79b741f72d5418f03d068a37859871
jahidul2004 <islamjahiduljihad@gmail.com>
2024-12-13 15:01:01 +0600

    Dynamically showing applied jobs compleate.

--- src/pages/myApplication/MyApplication.jsx
+++ src/pages/myApplication/MyApplication.jsx
@@ -15,6 +15,7 @@ const MyApplication = () => {
                 setJobs(data);
             });
     }, []);
+
     return (
         <div className="w-[95%] md:container mx-auto my-5">
             <h1 className="text-3xl font-bold text-center text-success mt-5 mb-10">
@@ -32,47 +33,48 @@ const MyApplication = () => {
                         </tr>
                     </thead>
                     <tbody>
-                        {/* row 1 */}
-                        <tr>
-                            <td>
-                                <div className="flex items-center gap-3">
-                                    <div className="avatar">
-                                        <div className="mask mask-squircle h-12 w-12">
-                                            <img
-                                                src="https://img.daisyui.com/images/profile/demo/2@94.webp"
-                                                alt="Avatar Tailwind CSS Component"
-                                            />
+                        {jobs?.map((job) => (
+                            <tr>
+                                <td>
+                                    <div className="flex items-center gap-3">
+                                        <div className="avatar">
+                                            <div className="mask mask-squircle h-12 w-12">
+                                                <img
+                                                    src={job.logo}
+                                                    alt="Avatar Tailwind CSS Component"
+                                                />
+                                            </div>
+                                        </div>
+                                        <div>
+                                            <div className="font-bold">
+                                                {job.company}
+                                            </div>
+                                            <div className="text-sm opacity-50">
+                                                {job.location}
+                                            </div>
                                         </div>
                                     </div>
-                                    <div>
-                                        <div className="font-bold">
-                                            Hart Hagerty
-                                        </div>
-                                        <div className="text-sm opacity-50">
-                                            United States
-                                        </div>
+                                </td>
+                                <td>
+                                    {job.title}
+                                    <br />
+                                    <span className="badge badge-ghost badge-sm">
+                                        {job.category}
+                                    </span>
+                                </td>
+                                <td>{job.name}</td>
+                                <td>
+                                    <div className="flex items-center gap-2">
+                                        <button className="btn btn-error">
+                                            <MdDeleteForever className="text-xl" />
+                                        </button>
+                                        <button className="btn btn-success font-bold text-white">
+                                            Details
+                                        </button>
                                     </div>
-                                </div>
-                            </td>
-                            <td>
-                                Zemlak, Daniel and Leannon
-                                <br />
-                                <span className="badge badge-ghost badge-sm">
-                                    Desktop Support Technician
-                                </span>
-                            </td>
-                            <td>Purple</td>
-                            <td>
-                                <div className="flex items-center gap-2">
-                                    <button className="btn btn-error">
-                                        <MdDeleteForever className="text-xl" />
-                                    </button>
-                                    <button className="btn btn-success font-bold text-white">
-                                        Details
-                                    </button>
-                                </div>
-                            </td>
-                        </tr>
+                                </td>
+                            </tr>
+                        ))}
                     </tbody>
                 </table>
             </div>
